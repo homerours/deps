708ea03b7cb62293c1ebddaf144664df37697603
homerours <homerours@gmail.com>
2023-08-15 22:01:21 +0100

    adding dotfiles

---
+++ dotfiles/README.md
@@ -0,0 +1,19 @@
+# dotfiles
+
+## Installation
+1. Clone
+`git clone https://github.com/homerours/dotfiles`
+
+1. Symlink + install additional stuff `source ./install.sh`
+
+## VIM
+Install Vim plugins (install vim-plug using install.sh before): 
+`:PlugInstall`
+
+## Deoplete
+Deoplete (completion for neovim) requires python3 support. Run
+`pip install neovim`
+`:UpdateRemotePlugins`
+
+## Neovim
+`:PackerInstall` and `:PackerSync`
---
+++ dotfiles/shellrc.sh
@@ -0,0 +1,25 @@
+export CLICOLOR=1
+export LSCOLORS="hx"
+
+# VIM as default
+if type nvim > /dev/null 2>&1
+then
+  editor='nvim'
+else
+  editor='vim'
+fi
+export EDITOR="$editor"
+export SUDO_EDITOR="$editor"
+export GIT_EDITOR="$editor"
+
+# Aliases
+source ~/dotfiles/aliases
+# Functions
+source ~/dotfiles/functions
+# Colors
+source ~/dotfiles/colors.sh
+# Git prompt
+source ~/dotfiles/git_prompt
+
+# include Z
+. ~/dotfiles/z.sh
